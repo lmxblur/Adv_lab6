4a7a651342afa7a5d719505c7c766f79c8e24954
lmxblur <lmxgt5@gmail.com>
2019-10-11 11:15:22 +0200

    Changes

--- lab6.Rcheck/lab6/doc/knapsack.R
+++
@@ -1,6 +0,0 @@
-## ----setup, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE, eval=FALSE)
-
-## ------------------------------------------------------------------------
-#  devtools::install_github("lmxblur/Adv_lab6", subdir="lab6")
-
