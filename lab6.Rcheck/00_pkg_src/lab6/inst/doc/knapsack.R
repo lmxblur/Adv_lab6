bb73dd009b147b50451cf3f9482047da66ea2e2d
spirosdim <spirosdmtr@gmail.com>
2019-10-11 09:22:02 +0200

    added brute force method

---
+++ lab6.Rcheck/00_pkg_src/lab6/inst/doc/knapsack.R
@@ -0,0 +1,6 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE, eval=FALSE)
+
+## ------------------------------------------------------------------------
+#  devtools::install_github("lmxblur/Adv_lab6", subdir="lab6")
+
---
+++ lab6/R/brute_force_knapsack.R
@@ -0,0 +1,61 @@
+brute_force_knapsack <- function(x, W, parallel=FALSE){
+  
+  if (!is.atomic(W) && length(W) != 1L) 
+    stop("W should be a scalar number")
+  if (W<=0 && W == Inf) 
+    stop("W should be a positive number")
+  if(!all(is.data.frame(x), 
+          ncol(x) == 2,
+          "v" %in% names(x),
+          "w" %in% names(x)))
+    stop('x should be a data.frame with two variables v and w')
+  if (!(all(x>0))) 
+    stop("x should have only positive values")
+  
+  bestV <- 0
+  n = nrow(x)
+  
+  # for(i in 0:(2^n-1)){
+  #   xi = intToBits(i)
+  #   sumv = 0     
+  #   sumw = 0     
+  #   tempv = c()   
+  #   for(j in 1:n){
+  #     if(xi[j] == 1){
+  #       sumv = sumv + x[j, ]$v
+  #       sumw = sumw + x[j, ]$w
+  #       tempv = c(tempv, j)
+  #     }
+  #   }
+  #   
+  #   if((sumv > bestV) && (sumw <= W)){
+  #     bestV = sumv
+  #     ev = tempv
+  #   }
+  # }
+  
+  if (parallel == FALSE) {
+    permut <- sapply(1:(2^n-1), function(i) { intToBits(i)[1:n]})
+    weights <- sapply(1:(2^n-1), function(i) { sum(as.numeric(permut[,i])*x$w) })
+    values <- sapply(1:(2^n-1), function(i) { sum(as.numeric(permut[,i])*x$v) })
+  }
+  else
+  {
+    cluster = parallel::makeCluster(parallel::detectCores() - 1)  # Calculate the number of cores & Initiate cluster
+    permut <- parallel::parSapply(cluster, 1:(2^n-1), function(i) {intToBits(i)[1:n]})
+    weights <- parallel::parSapply(cluster, 1:(2^n-1), function(i) {sum(as.numeric(permut[,i])*x$w) })
+    values <- parallel::parSapply(cluster, 1:(2^n-1), function(i) {sum(as.numeric(permut[,i])*x$v) })
+    parallel::stopCluster(cluster)  # close the cluster
+  }
+  for (i in 1:(2^n-1))  
+  {
+    if (bestV<values[i] && weights[i] <= W)
+    {
+      bestV <- values[i]
+      elements <- as.numeric(permut[,i])
+    }
+  }
+  return(list(value=round(bestV,0), elements=which(as.logical(elements))))
+  
+}
+  
